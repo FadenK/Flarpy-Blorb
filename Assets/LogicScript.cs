d7a7b2431b8fe7f888e3464cb552a9cdfe3dee8c
FadenK <JamesCalvinMeadersIII@gmail.com>
2023-05-04 14:40:01 -0400

    Migrated to TextMeshPro

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class LogicScript : MonoBehaviour
 {
   public int playerScore;
-  public Text scoreText;
+  public TMP_Text scoreText;
   public GameObject gameOverScreen;
 
   [ContextMenu("Increase Score")]
